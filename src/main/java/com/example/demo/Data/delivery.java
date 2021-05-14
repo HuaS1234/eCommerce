b7ec2a8c8d4e67ed7118e47f4104b4fb2e54a17a
Hua Shi <huashi2630@gmail.com>
2021-05-13 23:09:36 -0700

    added entity

--- src/main/java/com/example/demo/Data/delivery.java
+++ src/main/java/com/example/demo/Data/delivery.java
@@ -1,4 +1,64 @@
 package com.example.demo.Data;
 
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class delivery {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Nationalized
+    private String name;
+    @Column(name = "address_full", length = 500)
+    private String address;
+    private LocalDateTime deliveryTime;
+    @Type(type = "yes_no")
+    private Boolean completed;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDeliveryTime(LocalDateTime deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public LocalDateTime getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
 }
--- src/main/java/com/example/demo/Data/inventory.java
+++ src/main/java/com/example/demo/Data/inventory.java
@@ -1,5 +1,51 @@
 package com.example.demo.Data;
 
+import org.hibernate.annotations.Nationalized;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "plant")
 public class inventory {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Nationalized
+    private String name;
+    private String color;
+    @Column(precision = 12, scale = 4)
+    private BigDecimal price; // BigDecimal is the standard Java class for currency math
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
